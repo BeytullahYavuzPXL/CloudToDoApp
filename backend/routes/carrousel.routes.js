3070ca8b874930cb451fbf026ff019108d6b2b01
BeytullahYavuzPXL <beytullah.yavuz@student.pxl.be>
2023-05-12 19:32:17 +0200

    update

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -2,26 +2,21 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const express = require('express');
 const carrouselRouter = express.Router();
-//const carrouselData = require('./../data/carrousel.json');
 
 carrouselRouter.get('', (req, res) => {
-    res.json(carrouselData);
+    const bucketParams = {
+        Bucket: 'pe-2-static-assets',
+    };
+
+    s3.listObjects(bucketParams, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Failed to retrieve carrousel data' });
+        } else {
+            const carrouselData = data.Contents.map((object) => object.Key);
+            res.json(carrouselData);
+        }
+    });
 });
 
-
 module.exports = carrouselRouter;
-
-const bucketParams = {
-    Bucket: 'pe-2-static-assets',
-  };
-  
-  s3.listObjects(bucketParams, (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Objects in bucket:');
-      data.Contents.forEach((object) => {
-        console.log(object.Key);
-      });
-    }
-  });
